b17ca0e4a212f53c2d715b876a08a3420069e2a3
Chris Gracia <felemeria@gmail.com>
2023-11-10 16:30:09 -0500

    Menu working

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,44 @@
 import { Link } from 'react-router-dom';
-import React from 'react';
+import React, { useState } from 'react';
 import { IonIcon } from '@ionic/react';
-import { logoIonic } from 'ionicons/icons';
+import { logoFacebook, logoGithub, logoTwitter, menuOutline, closeOutline } from 'ionicons/icons';
 
 const Navbar: React.FC = () => {
+
+
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+
   return (
     <>
-      <div className="logo">
-        <a href="#"><img src="../assets/joyfulbuild.png" alt="Joyful Build Logo"></img></a>
+      <div className="top-bar">
+        <span className="logo">### ### ####</span>
+        <ul>
+          <li><Link to="#"><IonIcon icon={logoFacebook} aria-label="social-linkedin"></IonIcon></Link></li>
+          <li><Link to="#"><IonIcon icon={logoGithub} aria-label="social-github"></IonIcon></Link></li>
+          <li><Link to="#"><IonIcon icon={logoTwitter} aria-label="social-twitter"></IonIcon></Link></li>
+        </ul>
+
       </div>
-      <div className="toggle">
-        <a href="#"><IonIcon  name="{menu-outline}" aria-label="nav-menu-toggle"></IonIcon></a>
-      </div>
+
       <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/contact">Contact</Link>
+        <div className="logo">
+          <Link to="#"><img src="./src/assets/joyfulbuild.png" alt="Joyful Build Logo" /> Logo</Link>
+        </div>
+        <div className="toggle" onClick={toggleMobileMenu}>
+          <Link to="#"><IonIcon icon={isMobileMenuOpen ? closeOutline : menuOutline} aria-label="nav-menu-toggle"></IonIcon></Link>
+        </div>
+        <ul className={`menu ${isMobileMenuOpen ? 'open' : ''}`}>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
       </nav>
+
     </>
   );
 }
