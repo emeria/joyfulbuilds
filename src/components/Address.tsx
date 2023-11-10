7fd9166a9674de051b81a04989aea4904f229d0e
Chris Gracia <felemeria@gmail.com>
2023-11-10 17:35:24 -0500

    updated map

--- src/components/Address.tsx
+++ src/components/Address.tsx
@@ -4,9 +4,8 @@ const Address: React.FC = () => {
   return (
     <div className="address">
       <h3>Our Address</h3>
-      <p>1234 React Street</p>
-      <p>Code City, JS 12345</p>
-      <p>Country</p>
+      <p>Colchester, CT 06415</p>
+      <p>US</p>
     </div>
   );
 }
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Map: React.FC = () => {
-  const mapSrc = "https://www.google.com/maps/embed?pb=!1m18..."; // Replace with your actual Google Maps embed link
+  const mapSrc = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d95521.18461396215!2d-72.42559897369902!3d41.56844339867699!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89e6421cd4dc9509%3A0xafd80a6242e3c80d!2sColchester%2C%20CT%2006415!5e0!3m2!1sen!2sus!4v1699655585040!5m2!1sen!2sus"; // Replace with your actual Google Maps embed link
 
   return (
     <div className="map">
