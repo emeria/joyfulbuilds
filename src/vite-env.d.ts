9dd1cfdb3f42af2bfbc045362c065f845f91fb98
Chris Gracia <felemeria@gmail.com>
2023-11-12 15:44:46 -0500

    added .env

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -5,7 +5,6 @@ interface ImportMetaEnv {
     readonly EMAILJS_SERVICE_ID: string;
     readonly EMAILJS_TEMPLATE_ID: string;
     readonly EMAILJS_USER_ID: string;
-    // You can continue to add more variables here
   }
   
   interface ImportMeta {
