7ab677e78c90b5ed2ae73cb741dc966d13d4938a
Chris Gracia <felemeria@gmail.com>
2023-11-13 10:51:36 -0500

    basic grid and delimiter working

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,24 +3,24 @@ import '../styles/About.css';
 
 
 const languagesDatabases: string[] = ["C#", "Java", "PHP", "Python", "JS", "MySQL", "SQL Server", "HTML", "CSS"];
-const systemsPlatforms: string[]= ["Hadoop", "Docker", "Kubernetes", "Prometheus", "Databricks", "Spark", "Kafka"];
+const systemsPlatforms: string[] = ["Hadoop", "Docker", "Kubernetes", "Prometheus", "Databricks", "Spark", "Kafka"];
 const frameworksLibraries: string[] = ["AngularJS/7", "React", "ASP.NET MVC", "Open Layers", "Open StreetMaps", "Signal R", "Service Stack", "Liferay", "Drupal", "WordPress"];
 const devOpsTools: string[] = ["SSIS", "SSRS", "Visual Studio", "Pentaho", "BizTalk", "Mongo DB", "Eclipse", "PowerShell", "Office", "Terraform", "Ansible", "Packer", "ELK", "Jenkins", "Github", "Docksal", "TFS", "Jira", "Airflow"];
 
 const About: React.FC = () => {
 
-    const sortItems = (items: string[]): string[] => [...items].sort();
+  const sortItems = (items: string[]): string[] => [...items].sort();
 
-    const sortedLanguagesDatabases = useMemo(() => sortItems(languagesDatabases), [languagesDatabases]);
-    const sortedSystemsPlatforms = useMemo(() => sortItems(systemsPlatforms), [systemsPlatforms]);
-    const sortedFrameworksLibraries = useMemo(() => sortItems(frameworksLibraries), [frameworksLibraries]);
-    const sortedDevOpsTools = useMemo(() => sortItems(devOpsTools), [devOpsTools]);
+  const sortedLanguagesDatabases = useMemo(() => sortItems(languagesDatabases), [languagesDatabases]);
+  const sortedSystemsPlatforms = useMemo(() => sortItems(systemsPlatforms), [systemsPlatforms]);
+  const sortedFrameworksLibraries = useMemo(() => sortItems(frameworksLibraries), [frameworksLibraries]);
+  const sortedDevOpsTools = useMemo(() => sortItems(devOpsTools), [devOpsTools]);
 
-    const renderCategoryList = (items: string[]) => (
-      <div className="capability-category">
-        {items.map((item, index) => <span className="expertise-item" key={index}>{item}</span>)}
-      </div>
-    );
+  const renderCategoryList = (items: string[]) => (
+    <div className="capability-category">
+      {items.map((item, index) => <span className="capability-item" key={index}>{item}</span>)}
+    </div>
+  );
 
   return (
     <div>
@@ -29,30 +29,36 @@ const About: React.FC = () => {
 
       <h2 id="intro">Expertise</h2>
       <div className="expertise-container">
-        {['Problem Solving', 'Agile Development', 'Standardization', 'HIPAA/HITRUST','DevOps','Mentorship/Coaching','Communication','Teamwork/Collaboration','Accessibility (WCAG)','Security','Cloud (Azure, AWS)', ''].map((capability, index) =>
-          <span 
-          className="expertise-item" 
-          key={index}>{capability}</span>
+        {['Problem Solving', 'Agile Development', 'Standardization', 'HIPAA/HITRUST', 'DevOps', 'Mentorship/Coaching', 'Communication', 'Teamwork/Collaboration', 'Accessibility (WCAG)', 'Security', 'Cloud (Azure, AWS)'].map((capability, index) =>
+          <span
+            className="expertise-item"
+            key={index}>{capability}</span>
         )}
       </div>
 
-      <br/>
+      <br />
 
-      <h2 id="intro" className="capabilities-container">Capabilities</h2>
+      <h2 className="capabilities-heading">Capabilities</h2>
+      <div className="capabilities-container">
+        <div>
+          <h3>Languages & Databases</h3>
+          {renderCategoryList(sortedLanguagesDatabases)}
+        </div>
+        <div>
+          <h3>Systems & Platforms</h3>
+          {renderCategoryList(sortedSystemsPlatforms)}
+        </div>
+        <div>
+          <h3>Frameworks & Libraries</h3>
+          {renderCategoryList(sortedFrameworksLibraries)}
+        </div>
+        <div>
+          <h3>Development & Operations Tools</h3>
+          {renderCategoryList(sortedDevOpsTools)}
+        </div>
+      </div>
 
-      <h3>Languages & Databases</h3>
-      {renderCategoryList(sortedLanguagesDatabases)}
 
-      <h3>Systems & Platforms</h3>
-      {renderCategoryList(sortedSystemsPlatforms)}
-
-      <h3>Frameworks & Libraries</h3>
-      {renderCategoryList(sortedFrameworksLibraries)}
-
-      <h3>Development & Operations Tools</h3>
-      {renderCategoryList(sortedDevOpsTools)}
-
-     
     </div>
   );
 }
