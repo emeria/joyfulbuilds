a83c86698fde85d16a7c6b0540fe33cb212d8a99
Chris Gracia <felemeria@gmail.com>
2023-11-14 15:10:46 -0500

    added css battles and styling to hero button

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import { IonIcon } from '@ionic/react';
-import { logoLinkedin, logoGithub, logoTwitter } from 'ionicons/icons';
+import { logoLinkedin, logoGithub, logoTwitter, logoCss3 } from 'ionicons/icons';
 import '../styles/Footer.css';
 
 const Footer: React.FC = () => {
@@ -13,6 +13,10 @@ const Footer: React.FC = () => {
           <li><Link to="https://www.linkedin.com/in/christopher-gracia/"><IonIcon icon={logoLinkedin} aria-label="social-linkedin"></IonIcon></Link></li>
           <li><Link to="https://github.com/emeria"><IonIcon icon={logoGithub} aria-label="social-github"></IonIcon></Link></li>
           <li><Link to="https://twitter.com/cgracia"><IonIcon icon={logoTwitter} aria-label="social-twitter"></IonIcon></Link></li>
+          <li className='vertical-separator'></li>
+          <li><Link to="https://cssbattle.dev/player/emeria"><IonIcon icon={logoCss3} aria-label="social-css-battle"></IonIcon></Link></li>
+
+          
         </ul>
 
       </div>
