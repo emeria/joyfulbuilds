344f2430b03369dbb34b1eb117b13cb4a2031a15
Chris Gracia <felemeria@gmail.com>
2023-11-15 20:39:13 -0500

    added styling for portfolio and more data

--- src/components/PortfolioItem.tsx
+++ src/components/PortfolioItem.tsx
@@ -1,16 +1,36 @@
+
 // Hold portfolio items data
 interface PortfolioItemProps {
   title: string;
   description: string;
   link: string;
+  linkname: string;
+  technology: Array<string>;
 }
 
-const PortfolioItem: React.FC<PortfolioItemProps> = ({ title, description, link }) => {
+const PortfolioItem: React.FC<PortfolioItemProps> = ({ title, description, link, linkname, technology }) => {
+ 
+  const sortItems = (items: string[]): string[] => [...items].sort();
+
+  const renderTechnicalList = (items: string[]) => (
+    <ul className='tech-pill' aria-label='highlighted technology'>
+     {sortItems(items).map((item, index) => <li key={index}>{item}</li>)}
+    </ul>
+  );
+
+  
   return (
     <div className="portfolio-item">
-      <h3>{title}</h3>
+      <div className='portfolio-title-container'>
+        <h3>{title}</h3>
+        <div className="portfolio-title-line"></div>
+      </div>
       <p>{description}</p>
-      <a href={link} target="_blank" rel="noopener noreferrer">Learn More</a>
+      <a href={link} target="_blank" rel="noopener noreferrer">{linkname}</a>
+      <div className="tech-section">
+        <div className="tech-label">Technology</div>
+        {renderTechnicalList(technology)}
+      </div>
     </div>
   );
 }
--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -1,7 +1,3 @@
-import React from 'react'
-import PortfolioItem from '../components/PortfolioItem';
-import '../styles/Portfolio.css'
-
 export const portfolioData = [
     {
         title: 'CheatSheetMonster',
@@ -64,55 +60,3 @@ export const chrisPortfolioData = [
     },
 
 ];
-
-const Portfolio: React.FC = () => {
-
-
-    return (
-        <>
-            <div className='portfolio-page'>
-                <div className="portfolio-heading">
-                    <h1>Portfolio</h1>
-                    <p>Here you will find a list of projects that Joyful Build and Chris Gracia has worked on.</p>
-                </div>
-
-                <div className='portfolio-container'>
-                    <h2 className="portfolio-title">Joyful Build</h2>
-                
-                <div className="portfolio">
-                    {portfolioData.map(project => (
-                        <PortfolioItem
-                            key={project.title}
-                            title={project.title}
-                            description={project.description}
-                            link={project.link}
-                            linkname={project.linkname}
-                            technology={project.technology}
-                        />
-                    ))}
-                </div>
-                </div>
-                <br />
-
-                <div className='portfolio-container'>
-                <h2 className="portfolio-title">Chris Gracia</h2>
-                <div className="portfolio">
-                    {chrisPortfolioData.map(project => (
-                        <PortfolioItem
-                            key={project.title}
-                            title={project.title}
-                            description={project.description}
-                            link={project.link}
-                            linkname={project.linkname}
-                            technology={project.technology}
-                        />
-                    ))}
-                </div>
-                
-                </div>
-            </div>
-        </>
-    )
-}
-
-export default Portfolio
